54aa2ec79f386087cb3b01286dd13b53f83fe362
kohachiro <kohachiro@qq.com>
2012-09-27 14:08:15 +0800

    move framework to launcher

---
+++ src/launcher/launcheruv.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : launcheruv.cpp
+// Author      : kohachiro
+// Version     :
+// Copyright   : Copyright kohachiro, Inc. and other contributors. All rights reserved.
+// Description : Hello World in C, Ansi-style
+//============================================================================
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("Hello World");
+	return EXIT_SUCCESS;
+}
